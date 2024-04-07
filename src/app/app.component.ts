ada2cc3a71ce8e51b5849100395e5cbabcb21b5f
shmool <shmuelaj@gmail.com>
2024-04-07 10:54:11 +0300

    basic local implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+interface Event {
+  id: string,
+  title: string,
+  timestamp: Date
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'timestamp';
+  buttons = [
+    {id: '0', title: 'start working'},
+    {id: '1', title: 'finish working'},
+    {id: '2', title: 'baby sleeps'},
+    {id: '3', title: 'baby wakes'},
+  ]
+
+  log: Event[] = [];
+
+  logEvent ( title: string ) {
+    this.log.push({
+      id: this.log.length + '',
+      title,
+      timestamp: new Date()
+    })
+  }
+
+  addButton ( inputElement: HTMLInputElement ) {
+    this.buttons.push({
+      id: this.buttons.length + '',
+      title: inputElement.value
+    })
+    inputElement.value = ''
+  }
 }
