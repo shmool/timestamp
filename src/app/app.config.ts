0b6509d9f4b18161c7a513b1ea65d6e6fd36787d
shmool <shmuelaj@gmail.com>
2024-04-09 11:54:28 +0300

    add AngularFire and connect to Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"timestamp-sj","appId":"1:403380079015:web:adcf0b1c7e9362f142ce82","databaseURL":"https://timestamp-sj-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"timestamp-sj.appspot.com","apiKey":"AIzaSyAWk7w3yszuMjftuGREWou9TK0U1byfc7o","authDomain":"timestamp-sj.firebaseapp.com","messagingSenderId":"403380079015","measurementId":"G-QZGHZ8YTNH"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideDatabase(() => getDatabase()))]
 };
